efee952d696954b83a0ec8c4298406144a75170f
qordon <67695143+qordon@users.noreply.github.com>
2021-11-05 15:47:55 +0200

    Initial commit

---
+++ settings.py
@@ -0,0 +1,13 @@
+import enum
+
+
+class Team(enum.IntEnum):
+    # NaVi = 12
+    Vitality = 89
+    Gambit = 63
+    NiP = 1
+    VirtusPro = 31
+    Heroic = 95
+    G2 = 59
+    FaZe = 61
+    CopFlames = 101
